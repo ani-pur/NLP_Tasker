b6d4aec38654561b4a10b9016052bb3b5a531f58
ani-pur <anirudhpurohit1101@gmail.com>
2025-12-12 04:12:20 -0600

    Changed login redirect to test page

--- logic/main.py
+++ logic/main.py
@@ -31,7 +31,7 @@ def login():
             # ipAddr=request.remote_addr        # get from header: CF-Connecting-IP; current method will always return cf edge ip     
             # print("!! FAILED LOGIN FROM IP: ",ipAddr)
             print("[!] login attempted")        # will fix all this later i've got exams lol
-    return render_template('login.html', error=error)
+    return render_template('dual_login.html', error=error)
 
 # Detects if the incoming request is from a mobile device by checking the user-agent header for mobile keywords
 def is_mobile():        
