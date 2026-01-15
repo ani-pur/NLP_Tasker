759a2b52f15d5e751f83bb0c72e99534a90f19cb
ani-pur <anirudhpurohit1101@gmail.com>
2026-01-15 03:35:57 -0600

    Add debug output for user timezone metadata
    Added debug print statement for user timezone metadata.

--- logic/tasks_db.py
+++ logic/tasks_db.py
@@ -29,6 +29,9 @@ def add_task(username: str, jsonInput: str, task_data: dict):# added task_data a
     color = sendToDb.get('color')
     userInput = task_data.get('task_description')
     user_tz_metadata = str(task_data.get('user_tz_metadata', ''))	# user task submission timestamp
+    # debug
+    print("[DEBUG] tasks_db tz status: ",user_tz_metadata)
+    
     with dbConnect() as conn:
         with conn.cursor() as cur:
             try: 
