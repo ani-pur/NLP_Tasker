647aa93b82a1e87ad201f78cc04c43116852c4b4
ani-pur <anirudhpurohit1101@gmail.com>
2026-01-17 00:26:42 -0600

    Update tasks_db.py

--- logic/tasks_db.py
+++ logic/tasks_db.py
@@ -60,7 +60,7 @@ def get_all_tasks(username, sort_order):
             try: 
                 if sort_order=='default':
                     cur.execute(
-                        "SELECT * FROM tasks WHERE username = %s ORDER BY due_date ASC, task_time DESC;",(username,)
+                        "SELECT * FROM tasks WHERE username = %s ORDER BY due_date ASC, task_time ASC;",(username,)
                     )
                     rows = cur.fetchall()
                     return rows
